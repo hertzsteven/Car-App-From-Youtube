270253e7309d27bf697e5ac4958afc41ca62278c
Steven Hertz <StevenH@appi4.com>
2024-06-02 07:24:18 -0400

    Initial Commit

---
+++ Car App From Youtube/Car_App_From_YoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_App_From_YoutubeApp.swift
+//  Car App From Youtube
+//
+//  Created by Steven Hertz on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Car_App_From_YoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Car App From Youtube/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Car App From Youtube
+//
+//  Created by Steven Hertz on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
