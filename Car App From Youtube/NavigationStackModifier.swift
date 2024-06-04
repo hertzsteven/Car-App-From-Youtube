a778e231898cfc4b757375fb5192a31ef04752cb
Steven Hertz <StevenH@appi4.com>
2024-06-04 07:04:46 -0400

    using other

---
+++ Car App From Youtube/NavigationStackModifier.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationStackModifier.swift
+//  Car App From Youtube
+//
+//  Created by Steven Hertz on 6/2/24.
+//
+
+import SwiftUI
+
+struct NavigationStackModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationStackModifier()
+}
---
+++ Car App From Youtube/justcent.swift
@@ -0,0 +1,18 @@
+//
+//  justcent.swift
+//  Car App From Youtube
+//
+//  Created by Steven Hertz on 6/2/24.
+//
+
+import SwiftUI
+
+struct justcent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    justcent()
+}
